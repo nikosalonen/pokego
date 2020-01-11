697d6c41fee9422d1299596ab71021420b4fb07d
Niko Salonen <nikotsalonen@gmail.com>
2020-01-11 20:35:29 +0200

    fetch user data

--- pages/edit/_id.vue
+++ pages/edit/_id.vue
@@ -1,20 +1,45 @@
 <template>
-  <div class="container mx-auto">
-    Hello
-  </div>
+  <div class="container mx-auto">Hello {{ user.handle }}</div>
 </template>
 
 <script>
+import { fireDB } from '~/plugins/firestore.js'
 export default {
   components: {},
   data() {
     return {
       errors: [],
-      handle: '',
-      code1: '',
-      code2: '',
-      code3: '',
-      result: ''
+      user: {}
+    }
+  },
+  async asyncData({ params, error }) {
+    const user = await fireDB
+      .collection('users')
+      .doc(params.id)
+      .get()
+      .then((qs) => {
+        // // eslint-disable-next-line
+
+        return qs.data()
+      })
+    // eslint-disable-next-line
+    user.code1
+    return { user }
+  },
+  methods: {
+    async getUser() {
+      const handleFound = await fireDB
+        .collection('users')
+        .doc(this.$route.params.id)
+        .get()
+        .then((qs) => {
+          // // eslint-disable-next-line
+          // console.log(qs.size)
+          return qs.data()
+        })
+      // eslint-disable-next-line
+      debugger
+      return handleFound
     }
   }
 }
