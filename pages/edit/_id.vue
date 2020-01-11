8ccf3d0016ed7372f55639ce598eaf1c0cd22546
Niko Salonen <nikotsalonen@gmail.com>
2020-01-11 16:01:20 +0200

    add edit/_id

---
+++ pages/edit/_id.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container mx-auto">
+    Hello
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      errors: [],
+      handle: '',
+      code1: '',
+      code2: '',
+      code3: '',
+      result: ''
+    }
+  }
+}
+</script>
+
+<style></style>
