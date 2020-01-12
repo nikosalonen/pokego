aeca388b7ede91357a2859a1330c96eb3f3a2521
Niko Salonen <nikotsalonen@gmail.com>
2020-01-12 20:43:56 +0200

    fix minlengt typo, add checkLength

--- pages/index.vue
+++ pages/index.vue
@@ -31,27 +31,36 @@
               <div class="flex flex-wrap -mx-3 mb-2">
                 <div class="w-1/3 px-3 mb-6 md:mb-0">
                   <input
+                    id="code1"
+                    ref="code1"
+                    v-on:keyup="checkLength"
                     v-model="code1"
                     maxlength="4"
-                    nimlength="4"
+                    minlength="4"
                     type="text"
                     class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                   />
                 </div>
                 <div class="w-1/3 px-3 mb-6 md:mb-0">
                   <input
+                    id="code2"
+                    ref="code2"
                     v-model="code2"
+                    v-on:keyup="checkLength"
                     maxlength="4"
-                    nimlength="4"
+                    minlength="4"
                     type="text"
                     class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                   />
                 </div>
                 <div class="w-1/3 px-3 mb-6 md:mb-0">
                   <input
+                    id="code3"
+                    ref="code3"
                     v-model="code3"
+                    v-on:keyup="checkLength"
                     maxlength="4"
-                    nimlength="4"
+                    minlength="4"
                     type="text"
                     class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                   />
@@ -76,68 +85,6 @@
                   outlined
                 />
               </div>
-              <!--
-
-            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-              <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-city"
-              >
-                City
-              </label>
-              <input
-                id="grid-city"
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                type="text"
-                placeholder="Albuquerque"
-              />
-            </div>
-            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-              <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-state"
-              >
-                State
-              </label>
-              <div class="relative">
-                <select
-                  id="grid-state"
-                  class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                >
-                  <option>New Mexico</option>
-                  <option>Missouri</option>
-                  <option>Texas</option>
-                </select>
-                <div
-                  class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
-                >
-                  <svg
-                    class="fill-current h-4 w-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
-                    />
-                  </svg>
-                </div>
-              </div>
-            </div>
-            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-              <label
-                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                for="grid-zip"
-              >
-                Zip
-              </label>
-              <input
-                id="grid-zip"
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                type="text"
-                placeholder="90210"
-              />
-            </div>
-          </div> -->
             </form>
           </div>
 
@@ -191,6 +138,19 @@ export default {
     }
   },
   methods: {
+    checkLength(e) {
+      // eslint-disable-next-line
+      const id = e.target.id
+      const jump = e.target.value
+
+      if (jump.length === 4) {
+        if (id === 'code1') {
+          this.$refs.code2.focus()
+        } else if (id === 'code2') {
+          this.$refs.code3.focus()
+        }
+      }
+    },
     checkForm(e) {
       e.preventDefault()
       this.errors = []
