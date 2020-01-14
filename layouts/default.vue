da35657e3f78528b3dd0a45fc552c9a1cfe921e5
Niko Salonen <nikotsalonen@gmail.com>
2020-01-14 13:14:24 +0200

    add brave link

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mx-auto">
-    <div class="max-w-lg mx-auto px-2 md:p-10 bg-white rounded-lg shadow-xl">
+    <div class="max-w-lg mx-auto p-2 md:p-10 bg-white rounded-lg shadow-xl">
       <div class="flex flex-col sm:flex sm:items-center px-6 py-4">
         <div class="mt-4">
           <nuxt-link to="/">
@@ -13,6 +13,19 @@
         <div class="mt-4  text-center sm:text-left">
           <nuxt />
         </div>
+      </div>
+      <div class="w-1/2 flex m-auto pt-10">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://brave.com/pok897"
+        >
+          <img
+            src="~/static/switch_banner_1@2x.png"
+            alt="Download Brave Browser"
+            title="Download Brave Browser"
+          />
+        </a>
       </div>
     </div>
   </div>
