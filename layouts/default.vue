d5e5d4147c8e8ab2bcfc502332754d3eda47bea5
Niko Salonen <nikotsalonen@gmail.com>
2020-01-14 10:57:53 +0200

    mobile padding

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mx-auto">
-    <div class="max-w-lg mx-auto p-10 bg-white rounded-lg shadow-xl">
+    <div class="max-w-lg mx-auto px-2 md:p-10 bg-white rounded-lg shadow-xl">
       <div class="flex flex-col sm:flex sm:items-center px-6 py-4">
         <div class="mt-4">
           <nuxt-link to="/">
