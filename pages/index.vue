d8e618d21395b821e58fd4bdd58b1269c3572262
Niko Salonen <nikotsalonen@gmail.com>
2020-01-14 11:16:23 +0200

    mobile layout

--- pages/index.vue
+++ pages/index.vue
@@ -5,20 +5,34 @@
     </h1>
     <div class="flex items-center py-2">
       <div class="text-base text-gray-600 leading-tight w-full">
-        save my code as <br />
-        <span class="block mt-1 pt-1 text-lg flex justify-center"
-          >https://pokego.codes/
-          <div class="border-b border-b-2 border-gray-500">
-            <input v-model="handle" type="text" placeholder="AshKetchum" />
+        save my code as
+        <br />
+        <span
+          class="flex flex-wrap block mt-1 pt-1 text-lg flex justify-center w-full"
+        >
+          <div
+            class="w-full sm:w-1/2 block sm:inline-block sm:border-b sm:border-b-2 sm:border-gray-500"
+          >
+            https://pokego.codes/
+          </div>
+          <div
+            class="w-full sm:w-1/2 block sm:inline-block sm:border-b sm:border-b-2 sm:border-gray-500 text-2xl pt-5 sm:pt-0 sm:text-lg"
+          >
+            <input
+              v-model="handle"
+              class="text-center sm:text-left w-full block"
+              type="text"
+              placeholder="AshKetchum"
+            />
           </div>
         </span>
       </div>
     </div>
     <div class="mt-4 flex flex-col">
       <h2>My friend code is:</h2>
-      <form id="form" @submit="checkForm" class=" max-w-sm">
-        <div class="flex flex-wrap -mx-3 mb-2">
-          <div class="w-1/3 px-3 mb-6 md:mb-0">
+      <form id="form" @submit="checkForm" class="max-w">
+        <div class="flex flex-wrap  mb-2">
+          <div class="w-1/3 px-1 sm:px-3 mb-6 md:mb-0">
             <input
               id="code1"
               ref="code1"
@@ -30,7 +44,7 @@
               class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             />
           </div>
-          <div class="w-1/3 px-3 mb-6 md:mb-0">
+          <div class="w-1/3 px-1 sm:px-3 mb-6 md:mb-0">
             <input
               id="code2"
               ref="code2"
@@ -42,7 +56,7 @@
               class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             />
           </div>
-          <div class="w-1/3 px-3 mb-6 md:mb-0">
+          <div class="w-1/3 px-1 sm:px-3 mb-6 md:mb-0">
             <input
               id="code3"
               ref="code3"
@@ -69,7 +83,7 @@
           <input
             type="submit"
             value="Save"
-            class=" flex-shrink-0  bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+            class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
             color="primary"
             outlined
           />
@@ -96,7 +110,7 @@
           >
           <!-- <router-link to="https://pokego.codes/edit/{{ result }}"
                   >https://pokego.codes/edit/{{ result }}</router-link
-                > -->
+          >-->
           -- Don't lose it! There's no way to recover it.
         </p>
       </div>
