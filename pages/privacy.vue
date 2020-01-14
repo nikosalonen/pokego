900fc3d71e5e011246de0679252483e7dd396f39
Niko Salonen <nikotsalonen@gmail.com>
2020-01-14 19:29:23 +0200

    privacy page

---
+++ pages/privacy.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    This site uses
+    <a href="https://policies.google.com/privacy" target="_blank"
+      >Google Analytics</a
+    >
+    to analyze traffic. This site only collects the friend code and user handle,
+    nothing else.
+  </div>
+</template>
+<script>
+export default { layout: 'default', components: {}, data() {} }
+</script>
