4ab82e3b6e936a1548c9c1dcc56a5f98ff4a7821
Niko Salonen <nikotsalonen@gmail.com>
2020-01-14 14:10:12 +0200

    use .env

--- plugins/firestore.js
+++ plugins/firestore.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 if (!firebase.apps.length) {
   const config = {
-    apiKey: '***REMOVED***',
-    authDomain: 'pokego-codes.firebaseapp.com',
-    databaseURL: 'https://pokego-codes.firebaseio.com',
-    projectId: 'pokego-codes',
-    storageBucket: 'pokego-codes.appspot.com',
-    messagingSenderId: '136869508515',
-    appId: '1:136869508515:web:8cb0a817f1948e44089616',
-    measurementId: 'G-BXL4T7PDHM'
+    apiKey: process.env.FIRESTORE_APIKEY,
+    authDomain: process.env.FIRESTORE_AUTHDOMAIN,
+    databaseURL: process.env.FIRESTORE_DATABASEURL,
+    projectId: process.env.FIRESTORE_PROJECTID,
+    storageBucket: process.env.FIRESTORE_STORAGEBUCKET,
+    messagingSenderId: process.env.FIRESTORE_MESSAGINGSENDERID,
+    appId: process.env.FIRESTORE_APPID,
+    measurementId: process.env.FIRESTORE_MEASUREMENTID
   }
   firebase.initializeApp(config)
   firebase.firestore()
