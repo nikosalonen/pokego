0afa3b972d577eb5d2ede9c0f85aa89ad4a83853
Niko Salonen <nikotsalonen@gmail.com>
2020-01-14 11:51:36 +0200

    format link

--- pages/_id.vue
+++ pages/_id.vue
@@ -1,28 +1,30 @@
 <template>
   <div>
     <h1 class="text-xl text-gray-900 leading-tight">
-      Pokemon trainer <strong>{{ user.handle }}</strong> wants to be your
-      friend!
+      Pokemon trainer
+      <strong>{{ user.handle }}</strong> wants to be your friend!
     </h1>
     <div class="mt-4">
       <h2>The friend code is:</h2>
 
       <div class="flex flex-row justify-center items-stretch py-2">
         <span
-          class="shadow appearance-none border rounded  py-2 px-3 text-gray-700 leading-tight"
+          class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight"
+          >{{ user.code[0] }}-{{ user.code[1] }}-{{ user.code[2] }}</span
         >
-          {{ user.code[0] }}-{{ user.code[1] }}-{{ user.code[2] }}
-        </span>
       </div>
       <div class="flex flex-col items-center py-2 justify-center">
-        <p>
-          You can also use this QR code:
-        </p>
+        <p>You can also use this QR code:</p>
         <canvas
           id="canvas"
           ref="canvas"
           class="block object-contain h-48 w-full border border-solid"
         ></canvas>
+      </div>
+      <div class="flex flex-col items-center py-10 justify-center">
+        <nuxt-link class="underline pointer-events-auto cursor-pointer" to="/"
+          >Create your own link</nuxt-link
+        >
       </div>
     </div>
   </div>
@@ -70,9 +72,9 @@ export default {
         this.user.code,
         (error) => {
           // eslint-disable-next-line
-    if (error) console.error(error)
+          if (error) console.error(error)
           // eslint-disable-next-line
-    console.log('success!')
+          // console.log('success!')
         }
       )
     }
