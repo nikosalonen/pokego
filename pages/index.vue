e08cafb232e12de21e9dd2a3b20f31c6dc04e84e
Niko Salonen <niko.salonen@almamedia.fi>
2020-01-16 11:39:34 +0200

    wrong path in index page

--- pages/index.vue
+++ pages/index.vue
@@ -117,7 +117,7 @@
 </template>
 
 <script>
-import { fireDB } from '~/plugins/firestore.js'
+import { fireDB } from '~/plugins/firebase.js'
 
 export default {
   layout: 'default',
---
+++ plugins/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/analytics'
+if (!firebase.apps.length) {
+  const config = {
+    apiKey: process.env.FIRESTORE_APIKEY,
+    authDomain: process.env.FIRESTORE_AUTHDOMAIN,
+    databaseURL: process.env.FIRESTORE_DATABASEURL,
+    projectId: process.env.FIRESTORE_PROJECTID,
+    storageBucket: process.env.FIRESTORE_STORAGEBUCKET,
+    messagingSenderId: process.env.FIRESTORE_MESSAGINGSENDERID,
+    appId: process.env.FIRESTORE_APPID,
+    measurementId: process.env.FIRESTORE_MEASUREMENTID
+  }
+  firebase.initializeApp(config)
+  firebase.firestore()
+  firebase.analytics()
+}
+const fireDB = firebase.firestore()
+export { fireDB }
